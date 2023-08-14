204db06bb69445695d1ad221494010ed6b704d06
RohitPr <bladekiller95@gmail.com>
2023-08-14 12:31:37 +0530

    Update

--- Youtube Playlist/youtube.py
+++ Youtube Playlist/youtube.py
@@ -1,23 +1,67 @@
-from pytube import YouTube
-from pytube import Playlist
 import os
-import moviepy.editor as mp
-import re
+import yt_dlp
+from pytube import Playlist
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
 
-playlist = Playlist(
-    "https://www.youtube.com/playlist?list=PL4St63Dqz4Y0KRuznta3dWrqP45qSk1aL")
 
-for url in playlist:
-    print(YouTube(url).title)
-    YouTube(url).streams.filter(only_audio=True).first().download()
+def download_high_bitrate_audio(url, output_path):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+    }
 
-folder = "./"
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            print("Download successful:", info_dict['title'])
+    except yt_dlp.DownloadError as e:
+        print("Error during download:", e)
 
-for file in os.listdir(folder):
-    print(file)
-    if re.search('mp4', file):
-        mp4_path = os.path.join(folder, file)
-        mp3_path = os.path.join(folder, os.path.splitext(file)[0]+'.mp3')
-        new_file = mp.AudioFileClip(mp4_path)
-        new_file.write_audiofile(mp3_path)
-        os.remove(mp4_path)
+
+def get_video_urls_from_playlist(playlist_url):
+    try:
+        ydl_opts = {'quiet': True, 'extract_flat': True}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            playlist_info = ydl.extract_info(playlist_url, download=False)
+            if 'entries' in playlist_info:
+                return [entry['url'] for entry in playlist_info['entries']]
+    except yt_dlp.DownloadError as e:
+        print("Error:", e)
+    return []
+
+
+def convert_webm_to_mp3(input_file, output_file):
+    ffmpeg_extract_audio(input_file, output_file)
+
+
+if __name__ == "__main__":
+    playlist_url = "https://www.youtube.com/playlist?list=PL4St63Dqz4Y0KRuznta3dWrqP45qSk1aL"
+    download_path = "./Songs/"
+
+    video_urls = get_video_urls_from_playlist(playlist_url)
+
+    # if video_urls:
+    #     print("Video URLs in the playlist:")
+    #     for video_url in video_urls:
+    #         download_high_bitrate_audio(video_url, download_path)
+    # else:
+    #     print("No video URLs found in the playlist.")
+
+    input_folder = download_path  # Replace with the path to your folder containing WebM files
+    output_folder = "./conv_songs"  # Replace with the path where you want to save the MP3 files
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # List all files in the input folder
+    webm_files = [f for f in os.listdir(input_folder) if f.lower().endswith('.webm')]
+
+    for webm_file in webm_files:
+        input_file_path = os.path.join(input_folder, webm_file)
+        output_file_path = os.path.join(output_folder, os.path.splitext(webm_file)[0] + ".mp3")
+        
+        convert_webm_to_mp3(input_file_path, output_file_path)
+        print(f"Conversion complete: {webm_file} -> {os.path.basename(output_file_path)}")
+
+
--- bank_nifty_data/main.py
+++ bank_nifty_data/main.py
@@ -1,12 +1,9 @@
 from email import header
-import json
 from threading import local
-import datetime
 from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-import inspect
-import os.path
+
 
 spark = SparkSession \
     .builder \
@@ -19,4 +16,3 @@ read_df = spark.read.csv(
     './bank_nifty_data/banknifty.csv', header=True, inferSchema=True)
 
 read_df.show()
-
